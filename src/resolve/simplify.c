38bec89521587df1046c992411dce06d8ab1963f
Meilan39 <meilanmuto2005@gmail.com>
2025-03-30 18:30:57 +0900

    befiore massive changes to BNF

--- src/resolve/simplify.c
+++ src/resolve/simplify.c
@@ -11,57 +11,60 @@ E:  printf("...double overflow\n");
 
 int simplify_addition(Node* head) {
     for(int i = 0; i < head->length; i++) {
-        Node *temp, *ref, *rRational, *tRational;
-        double rn = 1, rd = 1, tn = 1, td = 1;
+        Node *this, *ref, *rTemp, *tTemp;
+        Node *rn = NULL, *rd = NULL, *tn = NULL, *td = NULL;
         int minus = 0;
         /* pass-over and mark signs */
         if(head->next[i]->type == lt_plus) {minus = 0; continue; }
         if(head->next[i]->type == lt_minus) {minus = 1; continue; } 
         /* remove parenthesis */
-        temp = n_get(head->next[i], nt_primary_expression);
-        if(temp->length == 3) {
-            n_delete(temp, 2, 0, 2);
+        this = n_get(head->next[i], nt_primary_expression);
+        if(this->length == 3) {
+            n_delete(this, 2, 0, 2);
         }
         /* get multiplication sign */
-        temp = n_get(head->next[i], nt_multiplicative_expression);
-        if(temp->next[0]->type == lt_minus || temp->next[0]->type == lt_plus) {
-            if(temp->next[0]->type == lt_minus)
+        this = n_get(head->next[i], nt_multiplicative_expression);
+        if(this->next[0]->type == lt_minus || this->next[0]->type == lt_plus) {
+            if(this->next[0]->type == lt_minus)
                 minus = !minus;
-            n_delete(temp, 1, 0);
+            n_delete(this, 1, 0);
         }
-        /* compress */
-        temp = n_get(head->next[i], nt_multiplicative_expression);
+        /* find match */
+        this = n_get(head->next[i], nt_multiplicative_expression);
         for(int j = 0; j < i; j+=2) {
-            int matched, rDivide = 0, tDivide = 0;
-            /* match against reference terms */
             ref = n_get(head->next[j], nt_multiplicative_expression);
-            for(int k = 0; k < ref->length; k+=2) {
-                matched = 0;
-                if(ref->next[k]->type == lt_dot) {rDivide = 0; continue;}
-                if(ref->next[k]->type == lt_slash) {rDivide = 1; continue;} 
-                if(n_get(ref->next[k], nt_rational)) {
+            int ridx = 0, tidx = 0;
+            while(1) {
+                if((rTemp = n_get(ref->next[ridx],  nt_rational)) ||
+                   (tTemp = n_get(this->next[tidx], nt_rational))) {
+                    if(rTemp) {
+                        if(!rn) rn = rTemp->next[0]; // ref numeraotr coefficient
+                        else    rd = rTemp->next[0]; // ref denominator coefficient
+                        ridx++;
+                    }
+                    if(tTemp) {
+                        if(!tn) tn = tTemp->next[0]; // temp numeraotr coefficient
+                        else    td = tTemp->next[0]; // temp denominator coefficient
+                        tidx++;
+                    }
                     continue;
                 }
-                for(int l = 0; l < temp->length; l+=2) {
-                    if(temp->next[l]->type == lt_dot) {tDivide = 0; continue;}
-                    if(temp->next[l]->type == lt_slash) {tDivide = 1; continue;} 
-                    if(n_get(temp->next[l], nt_rational)) {
-                        continue;                        
-                    }
-                    if(rDivide == tDivide && simplify_is_equal(ref->next[k], temp->next[l])) {
-                        matched = 1;
-                        break;
-                    }
-                }
-                if(!matched) {
-                    ref = NULL;
+                if(!simplify_is_equal(ref->next[ridx++], this->next[tidx++]) ||
+                   !(ridx < ref->length && tidx < this->length)) {
+                    ref = NULL; 
                     break;
                 }
             }
+            if(ref) break;
         }
-        /* merge temp into reference */
+        /* merge this into reference */
         if(ref) {
-            // need to make temp its own thing that can handle fractions.
+            double rnf, rdf, tnf, tdf;
+            rnf = rn ? rn->value : 1;
+            rdf = rd ? rd->value : 1;
+            tnf = tn ? tn->value : 1;
+            tdf = td ? td->value : 1;
+            // need to make this its own thing that can handle fractions.
             double coefficient = (rRational ? rRational->next[0]->value : 1) +
                                  (tRational ? tRational->next[0]->value : 1) *
                                  (minus ? -1 : 1);
